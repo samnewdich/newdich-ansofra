0f32a22ec7ae28d6a2a9963031b83010fb76a05a
samnewdich <samnewdich@gmail.com>
2026-01-14 19:05:36 +0100

    working on Auth

---
+++ newdichApp/Auth/AppAuthentication.php
@@ -0,0 +1,33 @@
+<?php
+namespace NewdichAuth;
+use NewdichSchema\Settings;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use PDO;
+use PDOException;
+
+ class AppAuthenticate{
+    private $jwtSecret = Settings::AUTH_KEY;
+    private $domain = Settings::DOMAIN_NAME;
+
+    public function auth($email){
+        if($email !==''){            
+            //NOW authorize $jwtSecret 
+            $authPayload = [
+                'iss'=>$this->domain, //provided in the tables file
+                'aud'=>$this->domain,
+                'iat'=>time(),
+                //'exp'=>time() + (24 * 60 * 60),
+                'user_id'=>trim($email),
+                'role'=>'user'
+            ];
+            $authhash = JWT::encode($authPayload, $this->jwtSecret, 'HS256');
+            return json_encode(array("status"=>"success", "response"=>$authhash), JSON_PRETTY_PRINT);
+        }
+        else{
+            //you can modify to add any role you want during authentication
+            return json_encode(array("status"=>"failed", "response"=>"user_id not provided"), JSON_PRETTY_PRINT);
+        }
+    }
+ }
+?>
---
+++ newdichApp/Auth/SrcAuthorization.php
@@ -0,0 +1,45 @@
+<?php
+namespace NewdichAuth;
+use NewdichSchema\Settings;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use PDO;
+use PDOException;
+
+class SrcAuthorization{
+    private $userToken;
+    private $user_id;
+    private $role;
+    private $expirationDate;
+    private $authResponse;
+    private $jwtSecret = Settings::AUTH_KEY;
+
+    public function __construct($userToken){
+        $this->userToken = $userToken;
+        try{
+            $decodeToken = JWT::decode($this->userToken, new Key($this->jwtSecret, 'HS256'), ['exp' => false]);
+            $this->user_id = $decodeToken->user_id;
+            $this->role = $decodeToken->role;
+            //$this->expirationDate = $decodeToken->exp;
+            $this->authResponse ="success";
+        }
+        catch(ExpiredException $e){
+            $this->authResponse ="Authorization token has expired, please relogin";
+        }
+        catch(Exception $e){
+            $this->authResponse = $e->getMessage();
+        }
+    }
+
+    public function authorize(){
+        if($this->role ==='admin'){
+            return json_encode(array("status"=>"success", "response"=>$this->authResponse), JSON_PRETTY_PRINT);
+        }
+        else{
+            //you can modify to add any role you want during authentication
+            return json_encode(array("status"=>"failed", "response"=>"access denied"), JSON_PRETTY_PRINT);
+        }
+    }
+
+}
+?>
