cfef50e8eb40791ea60801a37f514db949516063
samnewdich <samnewdich@gmail.com>
2026-02-26 16:07:41 +0100

    version 3.2.7

--- newdichApp/route/index.php
+++ newdichApp/route/index.php
@@ -21,11 +21,11 @@ $rootDir = Settings::ROOT_DIRECTORY; //the root directory of the project
 //for vtu, the root directory is /vtu and for fintech the root directory is /fintech
 //and if it is only one project you have, and the one project is inside (/var/www/html)
 // then the root directory will be /
-$usersArea = Settings::ROOT_DIRECTORY ."/api"; //the area that users can access
+$usersArea = $docRoot . $rootDir . Settings::ROOT_DIRECTORY ."/api"; //the area that users can access
 // let's say your root directory is / . Then the usersArea will be /api
 // if your root directory is /ecommerce, your usersArea will be /ecommerce/api
 // if your root directory is /vtu, your usersArea will be /vtu/api
-$adminArea = Settings::ROOT_DIRECTORY ."/apiadmin"; //the area that only admin can access
+$adminArea = $docRoot . $rootDir . Settings::ROOT_DIRECTORY ."/apiadmin"; //the area that only admin can access
 // let's say your root directory is /, your adminArea will be /apiadmin
 // if your root directory is /ecommerce, your adminArea will be /ecommerce/apiadmin
 //$appController = $serverDir.$rootDir."/Controller/App";
@@ -43,6 +43,13 @@ elseif($url === $usersArea || $url === $usersArea . "/"){
 elseif($url === $adminArea."/run_migration"){
     require_once __DIR__ . $srcController."/RunMigration.php";
     exit();
+}
+
+
+// /api endpoints
+elseif($url === $docRoot . $rootDir . $usersArea."/register" || $url === $usersArea."/register"."/"){
+    require_once __DIR__ . $appController."/RegisterController.php";
+    exit;
 }
 
 
